32084a3c3d7171986ce54777431bb7667d8bbdd6
Yevhenii Sarancha <yevhenii.sarancha@sigma.software>
2025-10-07 16:16:48 +0300

    removed filtering

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAdvert } from "../../redux/catalog/catalogOps";
 import {
@@ -6,7 +6,6 @@ import {
   selectIsLoading,
   selectHasError,
 } from "../../redux/catalog/catalogSelectors";
-import Filters from "../../components/Filters/Filters";
 import Catalog from "../../components/Catalog/Catalog";
 import Loader from "../../components/Loader/Loader";
 import css from "./CatalogPage.module.css";
@@ -16,79 +15,18 @@ const CatalogPage = () => {
   const items = useSelector(selectCatalogItems);
   const isLoading = useSelector(selectIsLoading);
   const hasError = useSelector(selectHasError);
-  const [filteredItems, setFilteredItems] = useState([]);
-  const [allFilteredItems, setAllFilteredItems] = useState([]);
-  const [brands, setBrands] = useState([]);
-  const [prices, setPrices] = useState([]);
   const [page, setPage] = useState(1);
-  const [filters, setFilters] = useState({});
 
   useEffect(() => {
     dispatch(fetchAdvert());
   }, [dispatch]);
 
-  const applyFilters = useCallback(
-    (filters, items) => {
-      let filtered = items;
-      if (filters.brand) {
-        filtered = filtered.filter((car) =>
-          car.make.toLowerCase().includes(filters.brand.toLowerCase())
-        );
-      }
-
-      if (filters.price) {
-        const maxPrice = parseInt(filters.price, 10);
-        filtered = filtered.filter(
-          (car) => parseInt(car.rentalPrice.replace(/\D/g, ""), 10) <= maxPrice
-        );
-      }
-
-      if (filters.mileageFrom) {
-        const minMileage = parseInt(filters.mileageFrom, 10);
-        filtered = filtered.filter((car) => car.mileage >= minMileage);
-      }
-
-      if (filters.mileageTo) {
-        const maxMileage = parseInt(filters.mileageTo, 10);
-        filtered = filtered.filter((car) => car.mileage <= maxMileage);
-      }
-
-      setAllFilteredItems(filtered);
-      setFilteredItems(filtered.slice(0, page * 12));
-    },
-    [page]
-  );
-
-  useEffect(() => {
-    if (items.length) {
-      const uniqueBrands = [...new Set(items.map((car) => car.make))];
-      setBrands(uniqueBrands);
-
-      const uniquePrices = [
-        ...new Set(
-          items.map((car) => parseInt(car.rentalPrice.replace(/\D/g, ""), 10))
-        ),
-      ].sort((a, b) => a - b);
-      setPrices(uniquePrices);
-
-      applyFilters(filters, items);
-    }
-  }, [items, filters, applyFilters]);
-
-  useEffect(() => {
-    applyFilters(filters, items);
-  }, [filters, items, page, applyFilters]);
-
-  const handleSearch = (filters) => {
-    setFilters(filters);
-    setPage(1);
-  };
-
   const loadMore = () => {
     setPage((prevPage) => prevPage + 1);
   };
 
-  const allItemsLoaded = filteredItems.length >= allFilteredItems.length;
+  const displayedItems = items.slice(0, page * 12);
+  const allItemsLoaded = displayedItems.length >= items.length;
 
   if (isLoading && page === 1) {
     return <Loader />;
@@ -100,10 +38,9 @@ const CatalogPage = () => {
 
   return (
     <div className="container">
-      <Filters onSearch={handleSearch} brands={brands} prices={prices} />
-      {filteredItems.length > 0 ? (
+      {displayedItems.length > 0 ? (
         <>
-          <Catalog items={filteredItems} />
+          <Catalog items={displayedItems} />
           {!allItemsLoaded && (
             <div className={css.btnContainer}>
               <button className={css.loadMore} onClick={loadMore}>
